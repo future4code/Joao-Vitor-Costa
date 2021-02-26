1d311a2f9d52d2d15e8007f32e33387c6a8f845c
joaovitorcostaa <joao.vitoramc@gmail.com>
2021-02-26 16:46:24 -0300

    Projeto JS

---
+++ semana3/blackjack/nossoJogo.js
@@ -0,0 +1,42 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+ 
+
+//  console.log("Bem vindo ao jogo de Blackjack!")
+
+//  if(confirm("Quer iniciar uma nova rodada?")) {
+
+//    let arrayCartas = [comprarCarta(), comprarCarta(), comprarCarta(), comprarCarta()]
+
+//    let usuarioCartas = arrayCartas[0].texto + arrayCartas[1].texto
+//    let computadorCartas = arrayCartas[2].texto + arrayCartas[3].texto
+//    let usuarioSoma = arrayCartas[0].valor + arrayCartas[1].valor
+//    let computadorSoma = arrayCartas[2].valor + arrayCartas[3].valor 
+ 
+//    console.log("Usuário - cartas:", usuarioCartas, " - pontuação: ", usuarioSoma)
+
+//    console.log("Computador - cartas:", computadorCartas, " - pontuação: ", computadorSoma)
+  
+//    if(usuarioSoma > computadorSoma) {
+//       console.log("O usuário ganhou!")
+//    } else if (usuarioSoma < computadorSoma) {
+//       console.log ("O computador ganhou!")
+//    } else if (usuarioSoma === computadorSoma) {
+//       console.log("Empate!")
+//    }
+// } else {
+//     console.log("O jogo acabou!")
+//  }
+
+ 
