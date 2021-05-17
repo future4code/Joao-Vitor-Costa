519860a832d66b44044e86f94bbec43069ee4115
Joao <joao.vitoramc@gmail.com>
2021-05-17 15:04:16 -0300

    desafios 1 e 2

--- semana15/node-package-template/2/index.js
+++ semana15/node-package-template/2/index.js
@@ -1,13 +1,22 @@
 const operacao = process.argv[2]
-const primeiroValor = process.argv[3]
-const segundoValor = process.argv[4]
+const primeiroValor = Number(process.argv[3])
+const segundoValor = Number(process.argv[4])
+const blue = "\033[34m"
 
+const add = primeiroValor + segundoValor
+const sub = primeiroValor - segundoValor
+const mult = primeiroValor * segundoValor
+const div =  primeiroValor / segundoValor
+
+if(operacao && primeiroValor && segundoValor){
 if(operacao === "add"){
-    console.log(Number(primeiroValor) + Number(segundoValor))
+    console.log(`${blue} ${add}`)
 } else if (operacao === "sub") {
-    console.log(Number(primeiroValor) - Number(segundoValor))
+    console.log(`${blue} ${sub}`)
 } else if (operacao === "mult") {
-    console.log(Number(primeiroValor) * Number(segundoValor))
+    console.log(`${blue} ${mult}`)
 } else if (operacao === "div") {
-    console.log(Number(primeiroValor) / Number(segundoValor))
+    console.log(`${blue} ${div}`)
+}} else if (!operacao || !primeiroValor || !segundoValor){
+    console.log("\033[34m Um ou mais parâmetros estão faltando!")
 }
